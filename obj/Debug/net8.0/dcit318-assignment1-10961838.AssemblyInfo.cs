c47467c080fabfb2c15f602fa89dc54a79d51024
Johnson <tandoh24700@gmail.com>
2024-06-02 02:09:25 +0000

    Grade Calculator

--- obj/Debug/net8.0/dcit318-assignment1-10961838.AssemblyInfo.cs
+++ obj/Debug/net8.0/dcit318-assignment1-10961838.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dcit318-assignment1-10961838")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+505f1014e91f159ff681f9450bf3c11227b632a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("dcit318-assignment1-10961838")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dcit318-assignment1-10961838")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
